b498146736601acdcb965172b500bde61f4fd017
Kasharid09 <denskt1@students.fscj.edu>
2026-07-30 13:36:08 -0400

    Add files via upload

---
+++ InvalidBirdDataException.java
@@ -0,0 +1,10 @@
+
+package edu.fscj.cop2800c.penguin;
+
+
+public class InvalidBirdDataException extends Exception {
+    public InvalidBirdDataException(String message){
+        super(message);
+    } 
+    
+}
